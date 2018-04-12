87ba281049ba633175b271c6a354206ffe6f31ba
riagoncalves <ricardo.goncalves252@gmail.com>
2018-04-12 11:35:07 +0100

    Code for implementing markdown information on a page

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
 
-const Project = ({title, image, body}) => (
-  <div className="project">
-    <h1>{title}</h1>
-    <img src={image} alt="Imagem do Projeto"/>
-    <p>{body}</p>
-  </div>
-)
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data; // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark;
+  return (
+    <div className="blog-post-container">
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </div>
+  );
+}
 
-export default Project
+export const pageQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`;
