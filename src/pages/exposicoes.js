ba758fb217a06f3da59fdb343327a5cc1395bdb9
riagoncalves <ricardo.goncalves252@gmail.com>
2018-05-03 11:51:28 +0100

    Other post pages

---
+++ src/pages/exposicoes.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PostLink from "../components/post-link";
+import Link from 'gatsby-link';
+
+const Exposicoes = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => edge.node.frontmatter.path.includes("/exposicoes")) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />);
+
+  { console.log(Posts) };
+  return <div>{Posts}</div>;
+};
+
+export default Exposicoes;
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
