01a70008e03000c61beb9fd5269751e7a8648917
riagoncalves <ricardo.goncalves252@gmail.com>
2018-05-09 14:43:23 +0100

    Added base template for new collections

---
+++ src/templates/imagePostTemplate.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data; // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark;
+  return (
+    <div className="project-container">
+      <div className="post">
+        <h1>{frontmatter.title}</h1>
+        <img src={frontmatter.image} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export const pageQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+        image
+      }
+    }
+  }
+`;
