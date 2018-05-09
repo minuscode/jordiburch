1613a3346f346ba8f41dcb61a1f6e35779a5d878
riagoncalves <ricardo.goncalves252@gmail.com>
2018-05-09 15:24:07 +0100

    Put english header paths

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,10 +6,10 @@ const Header = () => (
     <div className="container">
       <h1><Link to="/" className="home-btn">Jordi Burch</Link></h1>
       <div className="menu">
-        <p><Link to="/projetos">Projetos</Link></p>
-        <p><Link to="/livros">Livros</Link></p>
-        <p><Link to="/exposicoes">Exposições</Link></p>
-        <p><Link to="/textos">Textos</Link></p>
+        <p><Link to="/projects">Projects</Link></p>
+        <p><Link to="/books">Books</Link></p>
+        <p><Link to="/exhibitions">Exhibitions</Link></p>
+        <p><Link to="/texts">Texts</Link></p>
         <p><Link to="/cv">CV</Link></p>
       </div>
     </div>
