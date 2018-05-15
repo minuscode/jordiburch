9bb30affa5f8963dfdb4288891bee9263ecb97e5
riagoncalves <ricardo.goncalves252@gmail.com>
2018-05-15 11:08:42 +0100

    list all posts in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,38 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import PostLink from "../components/post-link";
+import Link from 'gatsby-link';
 
-const IndexPage = () => (
-  <div>
-  </div>
-)
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  console.log(edges);
+  const Posts = edges
+    .filter(edge => edge.node.frontmatter.templateKey !== 'previewGroupTemplate') // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />);
+    
+  return <div className="postsDisplay">{Posts}</div>;
+};
 
-export default IndexPage
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query IndexQueryPage {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+            image
+            templateKey
+          }
+        }
+      }
+    }
+  }
+`;
