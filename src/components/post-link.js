69a1593579c8c70de3e9bdb2bb20907cd125c7f4
riagoncalves <ricardo.goncalves252@gmail.com>
2018-05-15 11:16:39 +0100

    add check to write image div if image isn't null

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -5,10 +5,11 @@ const PostLink = ({ post }) => (
   <div className="preview-post">
     <Link to={post.frontmatter.path}>
       <div className="container">
-        
-        <div className="image">
-          <img src={post.frontmatter.image} alt="" />
-        </div>
+        {post.frontmatter.image !== null &&
+          <div className="image">
+            <img src={post.frontmatter.image} alt="" />
+          </div>
+        }
         <div className="text">
           <h1>{post.frontmatter.title}</h1>
           {post.frontmatter.image === null && <p>{post.excerpt}</p>}
