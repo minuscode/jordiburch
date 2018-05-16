5f4e1f7d89bb7c12f9d0c758b7612df70f43c420
riagoncalves <ricardo.goncalves252@gmail.com>
2018-05-16 17:13:03 +0100

    remove cv from post list on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PostLink from "../components/post-link";
 import Link from 'gatsby-link';
+import { EDEADLK } from "constants";
 
 const IndexPage = ({
   data: {
@@ -9,7 +10,7 @@ const IndexPage = ({
 }) => {
   console.log(edges);
   const Posts = edges
-    .filter(edge => edge.node.frontmatter.templateKey !== 'previewGroupTemplate') // You can filter your posts based on some criteria
+    .filter(edge => edge.node.frontmatter.templateKey !== 'previewGroupTemplate' && edge.node.frontmatter.path !== '/cv') // You can filter your posts based on some criteria
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />);
     
   return <div className="postsDisplay">{Posts}</div>;
