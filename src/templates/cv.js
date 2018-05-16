df44f322414e6281f4f0ebe77f368a26ccbbf148
riagoncalves <ricardo.goncalves252@gmail.com>
2018-05-16 16:08:45 +0100

    cv added

---
+++ src/templates/cv.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data; // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark;
+  return (
+    <div className="project-container">
+      <div className="post">
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export const pageQuery = graphql`
+  query CVPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`;
