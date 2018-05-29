7af915c20181ad6b2e41313f3c4783be45cbaf39
riagoncalves <ricardo.goncalves252@gmail.com>
2018-05-29 14:17:20 +0100

    remove P from generated images of backoffice markdown and add lightbox

--- src/templates/imagetextPostTemplate.js
+++ src/templates/imagetextPostTemplate.js
@@ -1,22 +1,61 @@
 import React from "react";
 
-export default function Template({
+function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data; // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark;
+
+  if (typeof window !== `undefined`) {
+    let divRandom = document.createElement('div');
+    let divContainer = document.createElement('div');
+    divContainer.className = "markdownContainer";
+    divRandom.appendChild(divContainer);
+    divContainer.innerHTML = data.markdownRemark.html;
+    let imgTags = Array.from(divContainer.querySelectorAll('p > img'));
+    console.log(imgTags);
+    if(imgTags.length !== 0) {
+      divContainer.innerHTML = '';
+      for (let i = 0; i < imgTags.length; i++) {
+        ((imgTags[i].alt === 'null') && (imgTags[i].alt = ''));
+        divContainer.innerHTML = divContainer.innerHTML + `
+          <div class="markdownImage">
+            <a href="${'#' + imgTags[i].src}" class='small-img'>
+              <div class="container">
+                  <div class="image">
+                    <img src="${imgTags[i].src}" alt="" />
+                  </div>
+
+                  <div class="text imagePostBox">
+                    <div class="descriptionBox"><p>${imgTags[i].alt}</p></div>
+                  </div>
+              </div>
+            </a>
+            <a href='#_' class='lightbox' id="${imgTags[i].src}">
+              <img src="${imgTags[i].src}" />
+            </a>
+          </div>
+        `;
+      }
+      data.markdownRemark.html = divRandom.innerHTML;
+    }
+    divRandom.remove();
+  }
+
   return (
     <div className="project-container">
       <div className="post">
         <h1 className="pageTitle">{frontmatter.title}</h1>
         <div
           className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
+          dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
         />
       </div>
     </div>
   );
 }
+
+export default Template;
 
 export const pageQuery = graphql`
   query ImageTextPostByPath($path: String!) {
