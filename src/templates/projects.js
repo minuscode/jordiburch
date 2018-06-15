b52804591c3c6afe12e4ecef85d0eb76387131e1
riagoncalves <ricardo.goncalves252@gmail.com>
2018-06-15 15:59:20 +0100

    added scroll inside lightbox

--- src/templates/projects.js
+++ src/templates/projects.js
@@ -22,7 +22,7 @@ function Template({
         ((imgTags[i].alt === 'null') && (imgTags[i].alt = ''));
         divContainer.innerHTML = divContainer.innerHTML + `
           <div class="markdownImage" class="hide" onload="this.classList.remove('hide')">
-            <a href="${'#' + imgTags[i].src}" class='small-img'>
+            <a href="${'#' + i}" class='small-img'>
               <div class="container">
                   <div class="image">
                     <img src="${imgTags[i].src}" alt="" class="hide" onload="this.classList.remove('hide')" />
@@ -33,10 +33,18 @@ function Template({
                   </div>
               </div>
             </a>
-            <a href='#_' class='lightbox' id="${imgTags[i].src}">
-              <img src="${imgTags[i].src}" class="hide" onload="this.classList.remove('hide')" />
-              <div class="descriptionBox"><p>${imgTags[i].alt}</p></div>
-            </a>
+            <div class="lightbox" id="${i}"">
+              <a href='#_' class='link'>
+                <img src="${imgTags[i].src}" class="hide" onload="this.classList.remove('hide')" >
+                <div class="descriptionBox"><p>${imgTags[i].alt}</p></div>
+              </a>
+              <a href="#${i+1}" class="arrow" id="${i}">
+                <img src="https://i.imgur.com/o7Fiap8.png"/>
+              </a>
+              <a href="#${i - 1}" class="arrow-invert" id="${i}">
+                <img src="https://i.imgur.com/o7Fiap8.png"/>
+              </a>
+            </div>
           </div>
         `;
       }
