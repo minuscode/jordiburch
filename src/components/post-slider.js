c433b5a86ddd3735828d6502935c1edcf246b27c
riagoncalves <ricardo.goncalves252@gmail.com>
2019-08-26 14:59:37 +0100

    slider fix

--- src/components/post-slider.js
+++ src/components/post-slider.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import { Carousel } from 'react-responsive-carousel';
-import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
+import('react-responsive-carousel/lib/styles/carousel.min.css');
 
 
 const PostSlider = ({ post }) => (
