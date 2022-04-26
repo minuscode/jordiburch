ca7bf99c36c9626517c4615ca71494f739630fec
Ricardo Gonçalves <ricardo.goncalves252@gmail.com>
2022-04-26 16:50:30 +0100

    fix(post-slider): images path

--- src/components/post-slider.js
+++ src/components/post-slider.js
@@ -1,36 +1,37 @@
 import React from "react";
 import { Link } from "gatsby";
 import { Carousel } from 'react-responsive-carousel';
+import { marked } from "marked";
 import('react-responsive-carousel/lib/styles/carousel.min.css');
 
 
-const PostSlider = ({ post }) => (
-  <div className="preview-post">
-   
-    <Link to={post.frontmatter.path}>
+const PostSlider = ({ post }) => {
+  const SliderWrap = post.frontmatter.featured === 'Yes' ? React.Fragment : Link
 
-      <div className="container">
-        <div className="image">
-          <Carousel autoPlay interval={10000} infiniteLoop showArrows={false} showIndicators={false} showThumbs={false} showStatus={false} dynamicHeight>
-            {post.frontmatter.images != null && (post.frontmatter.images.split('(').join(')').split(')').map(img => (
-              img.includes('/assets/images/uploads') &&
-              <div key={post.frontmatter.title}>
-                <img src={img} alt={post.frontmatter.title} />
-              </div>
-            ))).filter((element) => element !== false)}
-          </Carousel>
+  return (
+    <div className="preview-post">
+      <SliderWrap {...(post.frontmatter.featured === 'Yes' ? {} : { to: post.frontmatter.path})}>
+        <div className="container">
+          <div className="image">
+            {console.log(post)}
+            <Carousel autoPlay interval={10000} infiniteLoop showArrows={false} showIndicators={false} showThumbs={false} showStatus={false} dynamicHeight>
+              {post.frontmatter.images != null && (post.frontmatter.images.split('\n').map(img => (
+                img.includes('/assets/images/uploads') &&
+                <div key={post.frontmatter.title} dangerouslySetInnerHTML={{__html: marked.parse(img)}} />
+              ))).filter((element) => element !== false)}
+            </Carousel>
+          </div>
+
+          <div className="text imagePostBox">
+            <h1>{post.frontmatter.title}</h1>
+            {post.excerpt !== null && <div className="descriptionBox"><p>{post.excerpt}</p></div>}
+          </div>
         </div>
 
-        <div className="text imagePostBox">
-          <h1>{post.frontmatter.title}</h1>
-          {post.excerpt !== null && <div className="descriptionBox"><p>{post.excerpt}</p></div>}
-        </div>
-      </div>
+      </SliderWrap>
 
-    </Link>
-
-  </div>
-
-);
+    </div>
+  );
+}
 
 export default PostSlider;
